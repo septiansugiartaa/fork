d018af1c220792a32fecdb483e8337d627ac5987
dianingtass <dianingtas@gmail.com>
2026-03-02 01:05:34 +0700

    Fix login
    Handle akun multirole + add show password button

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -5,6 +5,7 @@ const {verifyToken} = require('../../src/middleware/verifyToken');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/finalize-login', authController.finalizeLogin);
 router.get('/me', verifyToken, authController.getCurrentUser);
 
 module.exports = router;
